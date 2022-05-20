3a5db6d5e4c1a8d1ef805e4da1c1c3d0dca3459d
JoshuaPeters21199 <jgp2.11.99@gmail.com>
2022-05-20 12:44:22 -0600

    Running into error with installing firebase using npm.

--- src/Feed.js
+++ src/Feed.js
@@ -1,11 +1,19 @@
-import React from 'react'
-import './Feed.css'
-import TweetBox from './TweetBox.js'
-import './TweetBox.css'
-import Post from './Post.js'
+import React, { useEffect, useState } from 'react';
+import './Feed.css';
+import TweetBox from './TweetBox.js';
+import './TweetBox.css';
+import Post from './Post.js';
+import db from './firebase';
 
 function Feed() {
   const [posts, setPosts]  = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapShot(snapshot =>(
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    ))
+  }, []);
+
   return (
     <div className='feed'>
       <div className='feed__header'>
@@ -13,7 +21,17 @@ function Feed() {
       </div>
 
       <TweetBox />
-      <Post />
+
+    {posts.map(post => (
+    <Post
+    displayName={post.displayName}
+    username={post.username}
+    verified={post.verified}
+    text={post.text}
+    avatar={post.avatar}
+    image={post.image}
+    />
+    ))}
     </div>
   )
 }
